7b4e3d957f39141ed0e374c5abfaebee02e9130d
huuthanhgg <109274579+huuthanhgg@users.noreply.github.com>
2025-03-13 18:58:33 +0700

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,7 +1,7 @@
--- AutoFarm GUI v2 by huuthanhgg
+-- AutoFarm GUI v2 by huuthanhgg (Thunder Spear Update)
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 local Window = Rayfield:CreateWindow({
-    Name = "AutoFarm - AOT",
+    Name = "AutoFarm - AOT (Thunder Spear)",
     LoadingTitle = "Loading Config...",
     LoadingSubtitle = "by huuthanhgg",
     ConfigurationSaving = {
@@ -48,6 +48,28 @@ MainTab:CreateToggle({
     end
 })
 
+-- Thunder Spear AutoFarm
+MainTab:CreateToggle({
+    Name = "Auto Thunder Spear",
+    CurrentValue = false,
+    Flag = "ThunderSpear",
+    Callback = function(state)
+        autoThunder = state
+    end
+})
+
+-- Thunder Spear Shot Count
+MainTab:CreateSlider({
+    Name = "Thunder Spear Shots",
+    Range = {1, 10},
+    Increment = 1,
+    CurrentValue = 5,
+    Flag = "SpearShots",
+    Callback = function(value)
+        spearShots = value
+    end
+})
+
 -- Speed Slider
 MainTab:CreateSlider({
     Name = "Fire Speed",
@@ -76,5 +98,25 @@ MiscTab:CreateButton({
         game:GetService("TeleportService"):Teleport(game.PlaceId)
     end
 })
+
+-- Function for Thunder Spear AutoFarm
+function autoFarm()
+    while autoRunning do
+        local boss = findBoss()
+        if boss and autoThunder then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = boss.CFrame
+            wait(0.1)
+            game:GetService("VirtualInputManager"):SendKeyEvent(true, "LeftShift", false, game)
+            wait(0.2)
+            game:GetService("VirtualInputManager"):SendKeyEvent(true, "Two", false, game)
+            wait(0.2)
+            for i = 1, spearShots do
+                game:GetService("VirtualInputManager"):SendMouseButtonEvent(0, 0, 0, true, game, 1)
+                wait(shotSpeed)
+            end
+        end
+        wait(0.05)
+    end
+end
 
 Rayfield:LoadConfiguration()
